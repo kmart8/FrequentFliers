ce0bd1026020021f1d70b7794effa69344cfb42c
Bazil313 <chrcollins2012@gmail.com>
2020-04-29 05:33:25 -0400

    Made comments more consistent and added the upper/lower limits for layover duration

--- src/utils/Saps.java
+++ src/utils/Saps.java
@@ -3,10 +3,7 @@
  */
 package utils;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
 
 /**
  * @author blake
@@ -18,7 +15,7 @@ import java.time.ZonedDateTime;
  */
 public class Saps {
 	/**
-	 * Constant valueS used for server access
+	 * Identification used for server access
 	 */
 	public static final String TEAM_NAME = "FrequentFliers";
 
@@ -28,29 +25,58 @@ public class Saps {
 	public static final String SERVER_URL =  "http://cs509.cs.wpi.edu:8181/CS509.server/ReservationSystem";
 
 	/**
-	 * Constant values used for latitude and longitude range validation
+	 * Upper limit used for latitude validation
 	 */
 	public static final double MAX_LATITUDE = 90.0;
+	/**
+	 * Lower limit used for latitude validation
+	 */
 	public static final double MIN_LATITUDE = -90.0;
+	/**
+	 * Upper limit used for longitude validation
+	 */
 	public static final double MAX_LONGITUDE = 180.0;
+	/**
+	 * Lower limit used for longitude validation
+	 */
 	public static final double MIN_LONGITUDE = -180.0;
 
 	/**
-	 * Constant values used for passenger and layover range validation
+	 * Upper limit used for passenger validation
 	 */
 	public static final int MAX_PASSENGERS = 15;
+	/**
+	 * Lower limit used for passenger validation
+	 */
 	public static final int MIN_PASSENGERS = 1;
+	/**
+	 * Upper limit used for layover validation
+	 */
 	public static final double MAX_LAYOVERS = 2;
+	/**
+	 * Lower limit used for layover validation
+	 */
 	public static final double MIN_LAYOVERS = 0;
 
 	/**
-	 * Constant values used for seating selection
+	 * Names used for seating selection
 	 */
 	public static final  String[] SEATING_TYPES = {"Any","Coach","First Class"};
 
 	/**
-	 * Constant values used for date range validation
+	 * Lower limit used for date range validation
 	 */
 	public static final ZonedDateTime EARLIEST_DATE = ZonedDateTime.of(LocalDateTime.of(2020,5,1,0,0), ZoneId.ofOffset("GMT", ZoneOffset.ofHours(0)));
+	/**
+	 * Upper limit used for date range validation
+	 */
 	public static final ZonedDateTime LATEST_DATE = ZonedDateTime.of(LocalDateTime.of(2020,6,1,0,0), ZoneId.ofOffset("GMT", ZoneOffset.ofHours(0)));
+	/**
+	 * Lower limit used for layover time
+	 */
+	public static final Duration MIN_LAYOVER_TIME = Duration.ofMinutes(15);
+	/**
+	 * Upper limit used for layover time
+	 */
+	public static final Duration MAX_LAYOVER_TIME = Duration.ofHours(4);
 }
