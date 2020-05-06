b8d95fe0695ffa6ebade5444081895eb9e240afd
Bazil313 <chrcollins2012@gmail.com>
2020-05-06 01:37:41 -0400

    Created for javadoc. Should be last commit necessary.

---
+++ src/ui/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes which handle user interaction.
+ *
+ * Includes the viewer for displaying and accepting user input, the contoller for parsing and validating the input,
+ * and the model for saving the input
+ *
+ * @author Chris Collins
+ * @version 1.0 2020-03-06
+ * @since 2020-03-06
+ */
+
+package ui;
