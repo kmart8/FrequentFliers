c56b12e4947b92fc1e0cbfb5c4a6aef59929c9c9
Bazil313 <chrcollins2012@gmail.com>
2020-05-06 01:34:44 -0400

    Finalized JavaDoc. Should be the last commit necessary.

--- src/utils/package-info.java
+++ src/utils/package-info.java
@@ -1,8 +1,10 @@
 /**
- * 
- */
-/**
- * @author blake
+ * Tools which require software-wide access.
  *
+ * Includes System Adaptive Parameters, notification generation, and query building.
+ *
+ * @author Chris Collins
+ * @version 1.0 2020-02-24
+ * @since 2020-03-06
  */
 package utils;
