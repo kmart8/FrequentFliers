30cdd28a11afde37edd9420df82ef27f7b3b09c5
Bazil313 <chrcollins2012@gmail.com>
2020-05-06 00:56:10 -0400

    Cleaned up code and finalized JavaDoc. Should be the last commit necessary.

--- src/ui/UIModel.java
+++ src/ui/UIModel.java
@@ -6,7 +6,9 @@ import utils.Saps;
 import java.time.*;
 
 /**
- * This class serves as a model for the ReservationApp viewer. It is controlled by the UIController.
+ * This class serves as a model for the ReservationApp viewer. It saves user input for future use.
+ * Note that input must be validated before submission to this model.
+ * TODO: Move the validation logic to this model
  *
  * @author Chris Collins
  * @version 2.0 2020-05-01
@@ -14,29 +16,46 @@ import java.time.*;
  *
  */
 public class UIModel {
-    // Initialize date type for specifying departure or arrival
+    /**
+     * Type for specifying departure or arrival
+     */
     private String timeType;
-
-    // Initialize empty times for departure or arrival
+    /**
+     * Beginning of the time window for departure or arrival
+     */
     private LocalTime startFlightLocalTime;
+    /**
+     * End of the time window for departure or arrival
+     */
     private LocalTime endFlightLocalTime;
-
-    // Initialize empty date for the flight
+    /**
+     * Date for the flight
+     */
     private LocalDate flightLocalDate;
-
-    // Initialize empty airports for departure and arrival
+    /**
+     * Requested departure airport
+     */
     private Airport departureAirport;
+    /**
+     * Requested arrival airport
+     */
     private Airport arrivalAirport;
-
-    // Initialize empty seating type
+    /**
+     * Type for specifying seating
+     */
     private String seatingType;
-
-    // Initialize the default number of passengers to 1
+    /**
+     * Requested number of passengers
+     */
     private int numberOfPassengers;
-
-    // Initialize the default number of layovers to 2
+    /**
+     * Maximum number of layovers
+     */
     private int numberOfLayovers;
 
+    /**
+     * Initializes the UIModel with default values for all fields except airports
+     */
     public UIModel(){
         timeType = "Departure";
         flightLocalDate = Saps.EARLIEST_DATE.toLocalDate();
@@ -46,6 +65,11 @@ public class UIModel {
         numberOfLayovers = 2;
     }
 
+    /**
+     * Get the beginning of the time window for the arrival or departure of the flight
+     *
+     * @return  The beginning of the time window as a ZonedDateTime in the GMT time zone, or null if arrival airport is empty
+     */
     public ZonedDateTime startFlightDateTime() {
         if (departureAirport != null)
             return departureAirport.convertLocalDateTimetoGMT(LocalDateTime.of(flightLocalDate, startFlightLocalTime));
@@ -53,6 +77,11 @@ public class UIModel {
             return null;
     }
 
+    /**
+     * Get the end of the time window for the arrival or departure of the flight
+     *
+     * @return  The end of the time window as a ZonedDateTime in the GMT time zone, or null if arrival airport is empty
+     */
     public ZonedDateTime endFlightDateTime() {
         if (arrivalAirport != null)
             return arrivalAirport.convertLocalDateTimetoGMT(LocalDateTime.of(flightLocalDate, endFlightLocalTime));
@@ -60,66 +89,156 @@ public class UIModel {
             return null;
     }
 
+    /**
+     * Get the beginning of the time window for the arrival or departure of the flight
+     *
+     * @return  The beginning of the time window as a LocalTime
+     */
     public LocalTime startFlightLocalTime() { return startFlightLocalTime; }
 
-    public LocalTime endFlightLocalTime() { return endFlightLocalTime; }
-
-    public LocalDate flightLocalDate() { return flightLocalDate; }
-
+    /**
+     * Set the beginning of the time window for the arrival or departure of the flight
+     *
+     * @param time The beginning of the time window as a LocalTime
+     */
     public void startFlightLocalTime(LocalTime time) { startFlightLocalTime = time; }
 
+    /**
+     * Get the end of the time window for the arrival or departure of the flight
+     *
+     * @return  The end of the time window as a LocalTime
+     */
+    public LocalTime endFlightLocalTime() { return endFlightLocalTime; }
+
+    /**
+     * Set the end of the time window for the arrival or departure of the flight
+     *
+     * @param time The end of the time window as a LocalTime
+     */
     public void endFlightLocalTime(LocalTime time) {
         endFlightLocalTime = time;
     }
 
+    /**
+     * Get the date for the flight
+     *
+     * @return  The date as a LocalDate
+     */
+    public LocalDate flightLocalDate() { return flightLocalDate; }
+
+    /**
+     * Set the date for the flight
+     *
+     * @param date The date as a LocalDate
+     */
     public void flightLocalDate(LocalDate date) {
         flightLocalDate = date;
     }
 
+    /**
+     * Get the type of time window the user is requesting (Arriving or Departing)
+     *
+     * @return The time type as a String
+     */
     public String timeType() {
         return timeType;
     }
 
+    /**
+     * Set the type of time window requested by the user (Arriving or Departing)
+     *
+     * @param dateType  The time type as a String
+     */
     public void timeType(String dateType) {
         this.timeType = dateType;
     }
 
+    /**
+     * Get the departure airport
+     *
+     * @return The departure Airport
+     */
     public Airport departureAirport() {
         return departureAirport;
     }
 
+    /**
+     * Set the departure airport
+     *
+     * @param departureAirport The departure Airport
+     */
     public void departureAirport(Airport departureAirport) {
         this.departureAirport = departureAirport;
     }
 
+    /**
+     * Get the arrival airport
+     *
+     * @return The arrival Airport
+     */
     public Airport arrivalAirport() {
         return arrivalAirport;
     }
 
+    /**
+     * Set the arrival airport
+     *
+     * @param arrivalAirport The arrival Airport
+     */
     public void arrivalAirport(Airport arrivalAirport) {
         this.arrivalAirport = arrivalAirport;
     }
 
+    /**
+     * Get the seating type
+     *
+     * @return The type seating as a String
+     */
     public String seatingType() {
         return seatingType;
     }
 
+    /**
+     * Set the seating type
+     *
+     * @param seatingType The type seating as a String
+     */
     public void seatingType(String seatingType) {
         this.seatingType = seatingType;
     }
 
+    /**
+     * Get the number of passengers
+     *
+     * @return The number of passengers as an Integer
+     */
     public int numberOfPassengers() {
         return numberOfPassengers;
     }
 
+    /**
+     * Set the number of passengers
+     *
+     * @param numberOfPassengers The number of passengers as an Integer
+     */
     public void numberOfPassengers(int numberOfPassengers) {
         this.numberOfPassengers = numberOfPassengers;
     }
 
+    /**
+     * Return the maximum number of layovers
+     *
+     * @return The maximum number of layovers as an Integer
+     */
     public int numberOfLayovers() {
         return numberOfLayovers;
     }
 
+    /**
+     * Set the maximum number of layovers
+     *
+     * @param numberOfLayovers The maximum number of layovers as an Integer
+     */
     public void numberOfLayovers(int numberOfLayovers) {
         this.numberOfLayovers = numberOfLayovers;
     }
