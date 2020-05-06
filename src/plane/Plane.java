1a2cc58554061c9c55470db3043856b11a07a0ec
wkeenan <wjkeenan@wpi.edu>
2020-05-06 11:10:06 -0400

    Removed to string from plane.

--- src/plane/Plane.java
+++ src/plane/Plane.java
@@ -44,10 +44,6 @@ public class Plane {
         mCoachSeats = Integer.MAX_VALUE;
     }
 
-    public String toString() {
-        return mManufacturer+" "+mModel+" "+mFirstClassSeats+" "+mCoachSeats;
-    }
-
     /**
      * Set the manufacturer for the plane
      *
