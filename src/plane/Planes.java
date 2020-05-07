95467c7b2cfdfcbfffe2e2055fb3d8a3ca6f8b54
wkeenan <wjkeenan@wpi.edu>
2020-05-07 11:09:41 -0400

    Updated Planes comments.

--- src/plane/Planes.java
+++ src/plane/Planes.java
@@ -6,8 +6,8 @@ package plane;
 import java.util.ArrayList;
 
 /**
- * This class aggregates a number of Airport. The aggregate is implemented as an ArrayList.
- * Airports can be added to the aggregate using the ArrayList interface. Objects can
+ * This class aggregates a number of Planes. The aggregate is implemented as an ArrayList.
+ * Planes can be added to the aggregate using the ArrayList interface. Objects can
  * be removed from the collection using the ArrayList interface.
  *
  * @author blake
